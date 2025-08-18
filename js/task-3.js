df6c0e8e9cc68c86e4b56a37e44533bc437f2786
Oksana Gura <gura.oxana@gmail.com>
2025-08-18 16:41:47 +0300

    my first js-hw

--- js/task-3.js
+++ js/task-3.js
@@ -2,13 +2,6 @@ function checkForSpam(message) {
   const lowerCaseMessage = message.toLowerCase();
     return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
     
-    checkForSpam("Latest technology news");
-    checkForSpam("JavaScript weekly newsletter");
-    checkForSpam("Get best sale offers now!");
-    checkForSpam("Amazing SalE, only tonight!");
-    checkForSpam("Trust me, this is not a spam message");
-    checkForSpam("Get rid of sPaM emails. Our book in on sale!");
-    checkForSpam("[SPAM] How to earn fast money?");
 }
 
 console.log(checkForSpam("Latest technology news")); // false
